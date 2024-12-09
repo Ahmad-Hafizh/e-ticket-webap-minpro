39090c33b4edf52851a4c68e5bf261a2b681c893
Ahmad Hafizh <140679217+Ahmad-Hafizh@users.noreply.github.com>
2024-12-10 06:37:14 +0700

    users signup (#2)

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -4,6 +4,8 @@ import ResponseHandler from '../utils/responseHandler';
 import { verify } from 'jsonwebtoken';
 import { transporter } from '../config/nodemailer';
 import { sign } from 'jsonwebtoken';
+import { compareSync } from 'bcrypt';
+import { hashPassword } from '../utils/hashPassword';
 
 export class UserController {
   async signUp(req: Request, res: Response, next: NextFunction): Promise<any> {
@@ -15,8 +17,9 @@ export class UserController {
         data: { referral_code, user_id: user.user_id },
       });
 
+      // parsing string to token
+      const authToken = sign({ email: user.email }, process.env.TOKEN_KEY || 'secretkey');
       // sending email with authtoken
-      const authToken = sign({ email: user.email }, 'secretkey');
       await transporter.sendMail({
         from: 'e-ticket',
         to: user.email,
@@ -24,10 +27,11 @@ export class UserController {
         html: `<div>
            <h1>Thank you ${user.name}, for registrater your account</h1>
            <p>klik link below to verify your account</p>
-           <a href='http://localhost:3005/users/verify/${authToken}'>Verify Account</a>
+           <a href='http://localhost:3000/users/verify-email?a_t=${authToken}'>Verify Account</a>
            </div>`,
       });
-      return ResponseHandler.success(res, 'sign up is success', 201, { user });
+
+      return ResponseHandler.success(res, 'sign up is success', 201, { ...user, authToken });
     } catch (error) {
       return ResponseHandler.error(res, 'sign up is failed', 500, error);
     }
@@ -36,14 +40,7 @@ export class UserController {
   // verifying account method
   async verifyEmail(req: Request, res: Response, next: NextFunction): Promise<any> {
     try {
-      // parsing token
-      // if the token passed to header
-      const authToken = req.headers.authorization || '';
-
-      // if the token passed to params
-      // const authToken = req.params.token;
-      const token: any = verify(authToken, 'secretkey');
-
+      const token = res.locals.dcrypt;
       // checking if user exist
       const isUserExist = await prisma.user.findUnique({
         where: { email: token.email },
@@ -62,6 +59,171 @@ export class UserController {
       return ResponseHandler.success(res, 'verfication is success', 200, user);
     } catch (error) {
       return ResponseHandler.error(res, 'verification is failed', 500, error);
+    }
+  }
+
+  async signIn(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const isUserExist = await prisma.user.findUnique({
+        where: { email: req.body.email },
+      });
+
+      if (!isUserExist) {
+        return ResponseHandler.error(res, 'account is not found', 404);
+      }
+
+      const comparePassword = compareSync(req.body.password, isUserExist.password);
+
+      if (!comparePassword) {
+        return ResponseHandler.error(res, 'Password is incorrect', 404);
+      }
+
+      const token = sign({ user_id: isUserExist.user_id, email: isUserExist.email }, process.env.TOKEN_KEY || 'secretkey');
+
+      return ResponseHandler.success(res, 'verfication is success', 200, {
+        name: isUserExist.name,
+        email: isUserExist.email,
+        pfp_url: isUserExist.pfp_url,
+        token,
+      });
+    } catch (error) {
+      return ResponseHandler.error(res, 'verification is failed', 500, error);
+    }
+  }
+
+  async keepLogin(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const userToken = res.locals.dcrypt;
+      const isUserExist = await prisma.user.findUnique({
+        where: { user_id: userToken.user_id, email: userToken.email },
+      });
+
+      if (!isUserExist) {
+        return ResponseHandler.error(res, 'account not exist', 404);
+      }
+
+      const newToken = sign({ user_id: isUserExist.user_id, email: isUserExist.email }, process.env.TOKEN_KEY || 'secretkey');
+
+      return ResponseHandler.success(res, 'keep login is success', 200, {
+        name: isUserExist.name,
+        email: isUserExist.email,
+        pfp_url: isUserExist.pfp_url,
+        newToken,
+      });
+    } catch (error) {
+      return ResponseHandler.error(res, 'keep login is failed', 500, error);
+    }
+  }
+
+  async updatePfp(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const userToken = res.locals.dcrypt;
+
+      const isUserExist = await prisma.user.findUnique({
+        where: { user_id: userToken.user_id, email: userToken.email },
+      });
+
+      if (!isUserExist) {
+        return ResponseHandler.error(res, 'account not exist', 404);
+      }
+
+      const user = await prisma.user.update({
+        where: { user_id: isUserExist.user_id },
+        data: { pfp_url: `/profile/${req.file?.filename}` },
+      });
+
+      return ResponseHandler.success(res, 'update profile picture is success', 201, user);
+    } catch (error) {
+      return ResponseHandler.error(res, 'update profile picture is failed', 500, error);
+    }
+  }
+
+  async updateProfile(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const userToken = res.locals.dcrypt;
+
+      const isUserExist = await prisma.user.findUnique({
+        where: { user_id: userToken.user_id, email: userToken.email },
+      });
+
+      if (!isUserExist) {
+        return ResponseHandler.error(res, 'account not exist', 404);
+      }
+
+      const profile = await prisma.profile.update({
+        where: { user_id: isUserExist.user_id },
+        data: { ...req.body },
+      });
+
+      return ResponseHandler.success(res, 'update profile is success', 201, profile);
+    } catch (error) {
+      return ResponseHandler.error(res, 'update profile is failed', 500, error);
+    }
+  }
+
+  async createAddress(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const userToken = res.locals.dcrypt;
+      console.log(userToken);
+
+      const isProfileExist = await prisma.profile.findUnique({
+        where: { user_id: userToken.user_id },
+      });
+
+      if (!isProfileExist) {
+        return ResponseHandler.error(res, 'account not exist', 404);
+      }
+
+      const address = await prisma.address.create({
+        data: { ...req.body, profile_id: isProfileExist.profile_id },
+      });
+      return ResponseHandler.success(res, 'update profile is success', 201, address);
+    } catch (error) {
+      return ResponseHandler.error(res, 'create address is failed', 500, error);
+    }
+  }
+
+  async forgotPassword(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const findUser = await prisma.user.findUnique({ where: { email: req.body.email } });
+      if (!findUser) {
+        return ResponseHandler.error(res, 'Account is not found', 404);
+      }
+
+      const authToken = sign({ email: findUser.email, user_id: findUser.user_id }, process.env.TOKEN_KEY || 'secretkey');
+      await transporter.sendMail({
+        from: 'e-ticket',
+        to: findUser.email,
+        subject: 'Password Recovery',
+        html: `<div>
+        <h1>Hi ${findUser.name}, Seems like you forgot your password</h1>
+        <p>klik link below to recover your password, if its not you try call police</p>
+        <a href='http://localhost:3000/users/recover-password?a_t=${authToken}'>recover password</a>
+        </div>`,
+      });
+      return ResponseHandler.success(res, 'An Email sended to your mail', 200);
+    } catch (error) {
+      return ResponseHandler.error(res, 'Your forgot password is failed', 500, error);
+    }
+  }
+
+  async recoverPassword(req: Request, res: Response, next: NextFunction): Promise<any> {
+    try {
+      const token = res.locals.dcrypt;
+      const findUser = await prisma.user.findUnique({
+        where: { user_id: token.user_id, email: token.email },
+      });
+      if (!findUser) {
+        return ResponseHandler.error(res, 'Account is not found', 404);
+      }
+      await prisma.user.update({
+        where: { user_id: findUser.user_id, email: findUser.email },
+        data: {
+          password: await hashPassword(req.body.password),
+        },
+      });
+    } catch (error) {
+      return ResponseHandler.error(res, 'Your forgot password is failed', 500, error);
     }
   }
 }
--- src/routers/user.routers.ts
+++ src/routers/user.routers.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controllers';
 import { createUser } from '../middlewares/createUser';
+import { signInValidator, signUpValidator } from '../middlewares/userValidator';
+import { verifyToken } from '../middlewares/verifyToken';
+import { uploader } from '../middlewares/uploader';
 
 export class UserRouter {
   private route: Router;
@@ -19,8 +22,18 @@ export class UserRouter {
 
   private initializeRouters(): void {
     // define every routes from controllers
-    this.route.post('/signup', createUser, this.userController.signUp);
-    this.route.post('/verify/:token', this.userController.verifyEmail);
+    this.route.post('/signup', signUpValidator, createUser, this.userController.signUp);
+    this.route.post('/signin', signInValidator, this.userController.signIn);
+
+    this.route.get('/keep-login', verifyToken, this.userController.keepLogin);
+    this.route.get('/verify', verifyToken, this.userController.verifyEmail);
+
+    this.route.patch('/update-pfp', verifyToken, uploader('/profile', 'PFP').single('imgProfile'), this.userController.updatePfp);
+    this.route.patch('/update-profile', verifyToken, this.userController.updateProfile);
+    this.route.post('/create-address', verifyToken, this.userController.createAddress);
+
+    this.route.post('/forgot-password', this.userController.forgotPassword);
+    this.route.patch('/recover-password', verifyToken, this.userController.recoverPassword);
   }
 
   // returning the routes so it can be use in app by calling it as a method
