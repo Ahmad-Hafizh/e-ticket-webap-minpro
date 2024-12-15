c3271c45d8bbd0a81dd8f9833e45a9bdabc09929
Ahmad-Hafizh <ahaaafizh@gmail.com>
2024-12-15 08:36:47 +0700

    user,organizer authentication and authorization middleware, user controller update

--- src/routers/user.routers.ts
+++ src/routers/user.routers.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/user.controllers';
-import { createUser } from '../middlewares/createUser';
+// import { createUser } from '../middlewares/createUser';
 import { signInValidator, signUpValidator } from '../middlewares/userValidator';
 import { verifyToken } from '../middlewares/verifyToken';
 import { uploader } from '../middlewares/uploader';
+import { organizerAuthorization } from '../middlewares/orgAuthor';
 
 export class UserRouter {
   private route: Router;
@@ -22,16 +23,22 @@ export class UserRouter {
 
   private initializeRouters(): void {
     // define every routes from controllers
-    this.route.post('/signup', signUpValidator, createUser, this.userController.signUp);
-    this.route.post('/signin', signInValidator, this.userController.signIn);
-
-    this.route.get('/keep-login', verifyToken, this.userController.keepLogin);
+    // new users
+    this.route.post('/signup', signUpValidator, this.userController.signUp);
+    this.route.patch('/add-referral', verifyToken, this.userController.addReferral);
     this.route.get('/verify', verifyToken, this.userController.verifyEmail);
 
+    // sign-in user
+    this.route.post('/signin', signInValidator, this.userController.signIn);
+    this.route.get('/keep-login', verifyToken, this.userController.keepLogin);
+
+    // update user & profile
     this.route.patch('/update-pfp', verifyToken, uploader('/profile', 'PFP').single('imgProfile'), this.userController.updatePfp);
+    this.route.patch('/update-role', verifyToken, this.userController.updateUserRole);
     this.route.patch('/update-profile', verifyToken, this.userController.updateProfile);
     this.route.post('/create-address', verifyToken, this.userController.createAddress);
 
+    // authentication prob
     this.route.post('/forgot-password', this.userController.forgotPassword);
     this.route.patch('/recover-password', verifyToken, this.userController.recoverPassword);
   }
